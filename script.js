30e614a44e93f35f40db009025f49fbbd97c5628
Laura Cecilia Müller <lacmuller@hotmail.com>
2021-08-29 15:51:25 -0300

    css

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-let nome = usuário
+--package json--
+
+document.getElementById(`nome`).textContent
+document.getElementById(`nascimento`).dataset
+estado_civil = document.getElementById(`estado civil`).textContent
+
+dados = {'nome': nome,
+                'nascimento': nascimento,
+                'Estado Civil': estado_civil,
+            }
+
+dados[nome] = nome 
+basededados['nascimento'] = nascimento           
+dados_id
+Basa de Dados
+MEMBROS = [ dados' ]
+basdededados[membros] = MEMBROS
+json.dump(basededados)
